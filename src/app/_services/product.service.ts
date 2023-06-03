7878fb4a4d4ab214f882113dc1cf3af564f652ae
guifroes1984 <guilhermefroes.web@gmail.com>
2023-06-02 23:26:11 -0300

    Segundo commit

---
+++ src/app/_services/product.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../_model/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public addProduct(product: FormData) {
+    return this.httpClient.post<Product>("http://localhost:8080/addNewProduct", product);
+  }
+
+  public getAllProducts() {
+    return this.httpClient.get<Product[]>("http://localhost:8080/getAllProducts");
+  }
+}
