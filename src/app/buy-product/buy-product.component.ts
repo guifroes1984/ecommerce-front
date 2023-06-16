ed05719f56f3de50350cc755646cbc27c915f259
guifroes1984 <guilhermefroes.web@gmail.com>
2023-06-16 11:29:33 -0300

    Gerando fatuara ao solicitar o produto com os cálculos

--- src/app/buy-product/buy-product.component.ts
+++ src/app/buy-product/buy-product.component.ts
@@ -50,4 +50,39 @@ export class BuyProductComponent implements OnInit {
       }
     );
   }
+
+  getQuantityForProduct(productId) {
+    const filteredProduct = this.orderDetails.orderProductQuantityList.filter(
+      (productQuantity) => productQuantity.productId === productId
+    );
+
+    return filteredProduct[0].quantity;
+  }
+
+  getCalculatedTotal(productId, productDiscountedPrice) {
+    const filteredProduct = this.orderDetails.orderProductQuantityList.filter(
+      (productQuantity) => productQuantity.productId === productId
+    );
+
+    return filteredProduct[0].quantity * productDiscountedPrice;
+  }
+
+  onQuantityChanged(q, productId) {
+    this.orderDetails.orderProductQuantityList.filter(
+      (orderProduct) => orderProduct.productId === productId
+    )[0].quantity = q;
+  }
+
+  getCalculatedGrandTotal() {
+    let grandTotal = 0;
+
+    this.orderDetails.orderProductQuantityList.forEach(
+      (producttQuantity) => {
+        const price = this.productDetails.filter(product => product.productId === producttQuantity.productId)[0].productDiscountedPrice;
+        grandTotal = grandTotal + price * producttQuantity.quantity;
+      }
+    );
+
+    return grandTotal;
+  }
 }
