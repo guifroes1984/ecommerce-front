6f473846313744e697be3e69ae496185cb53c7a0
guifroes1984 <guilhermefroes.web@gmail.com>
2023-06-30 17:15:27 -0300

    Adicionando barrara de pesquisa de produtos na página inicial

--- src/app/_services/product.service.ts
+++ src/app/_services/product.service.ts
@@ -14,8 +14,8 @@ export class ProductService {
     return this.httpClient.post<Product>("http://localhost:8080/addNewProduct", product);
   }
 
-  public getAllProducts(pageNumber) {
-    return this.httpClient.get<Product[]>("http://localhost:8080/getAllProducts?pageNumber=" + pageNumber);
+  public getAllProducts(pageNumber, searchKeyword: string = "") {
+    return this.httpClient.get<Product[]>("http://localhost:8080/getAllProducts?pageNumber=" + pageNumber + "&searchKey=" + searchKeyword);
   }
 
   public getProductDetailsById(productId) {
