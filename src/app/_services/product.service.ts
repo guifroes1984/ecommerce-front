e6808f0b3363fd31a74b6b0ed65fd3a0e0f041e9
guifroes1984 <guilhermefroes.web@gmail.com>
2023-07-12 20:19:01 -0300

    Fazendo toda a logica de esvaziar carrinho após o pedido feito

--- src/app/_services/product.service.ts
+++ src/app/_services/product.service.ts
@@ -30,8 +30,8 @@ export class ProductService {
     return this.httpClient.get<Product[]>("http://localhost:8080/getProductDetails/" + isSingleProductCheckout + "/" + productId);
   }
 
-  public placeOrder(orderDetails: OrderDetails) {
-    return this.httpClient.post("http://localhost:8080/placeOrder", orderDetails);
+  public placeOrder(orderDetails: OrderDetails, isCartCheckout) {
+    return this.httpClient.post("http://localhost:8080/placeOrder/" + isCartCheckout, orderDetails);
   }
 
   public addToCart(productId) {
